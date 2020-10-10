8fb3bc32bf6c1027ab95e8d0013946bddf130294
Pratik <pratikbambulkar1818@gmail.com>
2020-10-11 00:22:40 +0530

    Initial commit

---
+++ Decimal_convert_hex_bin_oct.py
@@ -0,0 +1,4 @@
+dec=int(input("Enter The Decimal: "))
+print("The Binary of Deciaml",dec,"is:",bin(dec)[2:])
+print("The Octa of Deciaml",dec,"is:",oct(dec)[2:])
+print("The Hexa of Deciaml",dec,"is:",hex(dec)[2:])
